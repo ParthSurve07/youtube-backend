e855282402df028dc116fe249f6803da159ff153
Parth Surve <surveparth429@gmail.com>
2025-10-24 16:48:29 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,20 +10,20 @@ app.use(cors(
     credentials: true
   }
 ))
-
 app.use(express.json(
   {
     limit: '16kb'
   }
 ))
-
 app.use(express.urlencoded({
   extended: true,
   limit: '16kb'
 }))
-
 app.use(express.static('public'))
-
 app.use(cookieParser())
+
+import userRouter from './routes/user.routes.js'
+
+app.use('/api/v1/users', userRouter)
 
 export default app
